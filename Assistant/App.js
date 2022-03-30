77a694dd55cd242f3e3ba1550053b8457438cd93
MateeGarcia <51801253+MateeGarcia@users.noreply.github.com>
2022-03-30 18:47:06 -0300

    Update App.js

--- Assistant/App.js
+++ Assistant/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Mate es un capo</Text>
+      <Text>Mate es un capoasd</Text>
       <StatusBar style="auto" />
     </View>
   );

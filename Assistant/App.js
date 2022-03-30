163239169857528bae8668514854493889905ba2
MateeGarcia <51801253+MateeGarcia@users.noreply.github.com>
2022-03-30 20:57:22 -0300

    Nada importante

--- Assistant/App.js
+++ Assistant/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>cris es un capoasd</Text>
+      <Text>puto el e lee</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
